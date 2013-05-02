2ec83cfe06fa45f5dc48ff985ca41560823d54fd
Sam Weinberg <samweinberg@me.com>
2013-05-02 01:18:06 -0700

    Fixed minor detail that was removed in recent merge

--- dialog_box.js
+++ dialog_box.js
@@ -6,11 +6,11 @@ var x;
 var r=confirm;javascript:window.prompt ('Please send a donation to our Bitcoin address:', addr);
 if (r)
   {
-  x="Thank you!";
+  x="Thank you!&#9829;";
   }
 else
   {
-  x="Thank you!";
+  x="Thank you!&#9829;";
   }
 document.getElementById("demo").innerHTML=x;
 }
